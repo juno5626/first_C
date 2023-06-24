6efb4bf6cede297b90413d96f3c627b9abf7be5d
Your Name <junono1@naver.com>
2023-06-24 22:55:04 +0900

    add more details

--- 1. hello world/hello.c
+++ 1. hello world/hello.c
@@ -1,6 +1,13 @@
-#include <stdio.h>
+#include <stdio.h> // header file
 
-int main(void){
+
+int main(void){ 
     printf("Hello world! \n");
     return 0;
 }
+
+/*
+program will be executed main function in order from top to bottom
+
+header file provide for code a essential 
+*/
